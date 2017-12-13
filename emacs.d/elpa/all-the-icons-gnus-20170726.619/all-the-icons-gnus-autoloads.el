b9ba059eb6d384061915526d441fa84f643eb28e
Exbilar <exbilar@gmail.com>
2017-12-13 21:44:57 +0800

    delete

--- emacs.d/elpa/all-the-icons-gnus-20170726.619/all-the-icons-gnus-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; all-the-icons-gnus-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "all-the-icons-gnus" "all-the-icons-gnus.el"
-;;;;;;  (23020 32142 346952 945000))
-;;; Generated autoloads from all-the-icons-gnus.el
-
-(autoload 'all-the-icons-gnus-setup "all-the-icons-gnus" "\
-Add icons for Gnus.
-
-\(fn)" nil nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; all-the-icons-gnus-autoloads.el ends here
--- emacs.d/elpa/s-20170922.900/s-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; s-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("s.el") (23020 32224 315245 650000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; s-autoloads.el ends here
