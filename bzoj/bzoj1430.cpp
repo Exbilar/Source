bf8b02a5252525d47ac153e46f632026f734701e
Exbilar <exbilar@gmail.com>
2017-12-30 22:18:20 +0800

    process

---
+++ bzoj/bzoj1430.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define DEBUG(...) fprintf(stderr,__VA_ARGS__)
+#define clr(x) memset(x,0,sizeof x)
+
+using std :: max;
+using std :: min;
+
+typedef long long LL;
+
+static const int INF = 1 << 30;
+static const LL oo = 1LL << 62;
+static const int maxm = 1e6 + 10;
+static const int mod = 9999991;
+
+template <class T> inline bool chkmax(T &x,T y){
+    return x < y ? x = y,true : false;
+}
+
+template <class T> inline bool chkmin(T &x,T y){
+    return x > y ? x = y,true : false;
+}
+
+template <class T> inline void read (T &in){
+    in = 0; char ch = getchar(); T f = 1;
+    for (;ch > '9' || ch < '0';ch = getchar()) if(ch == '-') f = -1;
+    for (;ch >= '0' && ch <= '9';ch = getchar()) in = in * 10 + ch - '0';
+    in = in * f;
+}
+
+LL n,ans = 1;
+
+int main(){
+	read(n);
+	for(int i = 1;i <= n - 2;i++) ans = ans * n % mod;
+	for(int i = 1;i <= n - 1;i++) ans = ans * i % mod;
+	printf("%lld\n",ans);
+
+	return 0;
+}
