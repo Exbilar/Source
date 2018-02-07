96d49c85b320bc03004bb77940403584cef73cd0
Exbilar <exbilar@gmail.com>
2018-02-07 11:18:24 +0800

    spoj

--- 51Nod/51nod1244.cpp
+++ 51Nod/51nod1244.cpp
@@ -2,76 +2,77 @@
 
 #define DEBUG(...) fprintf(stderr,__VA_ARGS__)
 #define clr(x) memset(x,0,sizeof x)
+#define RG register
 
 using std :: max;
 using std :: min;
-using std :: map;
+using std :: vector;
 
 typedef long long LL;
 
 static const int INF = 1 << 30;
 static const LL oo = 1LL << 62;
-static const int maxm = 5e6 + 10;
 
-template <class T> inline bool chkmax(T &x,T y){
+template <class T> inline bool chkmax (T &x, T y) {
     return x < y ? x = y,true : false;
 }
 
-template <class T> inline bool chkmin(T &x,T y){
+template <class T> inline bool chkmin (T &x, T y) {
     return x > y ? x = y,true : false;
 }
 
-template <class T> inline void read (T &in){
+template <class T> inline void read (T &in) {
     in = 0; char ch = getchar(); T f = 1;
-    for (;ch > '9' || ch < '0';ch = getchar()) if(ch == '-') f = -1;
+    for (;ch > '9' || ch < '0';ch = getchar()) if (ch == '-') f = -1;
     for (;ch >= '0' && ch <= '9';ch = getchar()) in = in * 10 + ch - '0';
     in = in * f;
 }
 
-int mu[maxm],prm[maxm];
-bool npr[maxm];
-LL M[maxm];
+LL l,r;
+int M;
 
-int cnt,num,lmt;
+vector<LL> pre,suf;
+vector<int> prm;
 
-map<LL,LL> h;
-
-LL getPrime(int N){
-    mu[1] = 1;
-    for(int i = 2;i <= N;i++){
-        if(!npr[i]) prm[++num] = i,mu[i] = -1;
-        for(int j = 1;j <= num && prm[j] * i <= N;j++){
-            npr[prm[j] * i] = 1;
-            mu[prm[j] * i] = -mu[i];
-            if(i % prm[j] == 0){
-                mu[prm[j] * i] = 0;
-                break;
-            }
-        }
-    }
-    for(int i = 1;i <= N;i++) M[i] = M[i - 1] + mu[i];
+LL dfs(LL rem,int lst,LL mul,LL n) {
+	LL tmp = (rem > M ? suf[n / rem] : pre[rem]) - pre[prm[lst] - 1];
+	LL res = -tmp * mul;
+	for(int i = lst;i < (int) prm.size();i++) {
+		int p = prm[i];
+		if(1LL * p * p > rem) break;
+		res += dfs(rem / p,i + 1,-mul,n);
+	}
+	return res;
 }
 
-LL calc(LL x){
-    if(x <= lmt) return M[x];
-    if(h.count(x)) return h[x];
-    LL res = 1;
-    for(LL l = 2,r = 0;l <= x;l = r + 1){
-        r = x / (x / l);
-        res -= calc(x / l) * (r - l + 1);
-    }
-    return h[x] = res;
+LL solve(LL n){
+	pre.clear(),suf.clear(),prm.clear();
+	
+	M = sqrt(n);
+	pre.resize(M + 1),suf.resize(M + 1);
+
+	for(int i = 1;i <= M;i++) {
+		pre[i] = i - 1;
+		suf[i] = n / i - 1;
+	}
+	
+	for(int p = 2;p <= M;p++) {
+		if(pre[p] == pre[p - 1]) continue;
+		prm.push_back(p);
+		LL pcnt = pre[p - 1],q = 1LL * p * p,m = n / p;
+		LL end = min((LL) M,n / q);
+		for(int i = 1,w = M / p;i <= w;i++) suf[i] -= suf[i * p] - pcnt;
+		for(int i = M / p + 1;i <= end;i++) suf[i] -= pre[m / i] - pcnt;
+		for(int i = M;i >= q;i--) pre[i] -= pre[i / p] - pcnt;
+	}
+	prm.push_back(M + 1);
+	return n > 1 ? 1 + dfs(n,0,1,n) : 1;
 }
 
-int main(){
+int main() {
+	read(l),read(r);
+
+	printf("%lld\n",solve(r) - solve(l - 1));
 	
-    LL l,r;
-	
-    read(l),read(r);
-	
-    getPrime(lmt = pow(r,2 / (double) 3));
-	
-    printf("%lld\n",calc(r) - calc(l - 1));
-	
-    return 0;
+	return 0;
 }
---
+++ spoj/DIVCNT2.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+#define DEBUG(...) fprintf(stderr,__VA_ARGS__)
+#define clr(x) memset(x,0,sizeof x)
+#define RG register
+
+using std :: max;
+using std :: min;
+using std :: vector;
+
+typedef long long LL;
+typedef unsigned long long ull;
+
+static const int INF = 1 << 30;
+static const LL oo = 1LL << 62;
+
+template <class T> inline bool chkmax (T &x, T y) {
+    return x < y ? x = y,true : false;
+}
+
+template <class T> inline bool chkmin (T &x, T y) {
+    return x > y ? x = y,true : false;
+}
+
+template <class T> inline void read (T &in) {
+    in = 0; char ch = getchar(); T f = 1;
+    for (;ch > '9' || ch < '0';ch = getchar()) if (ch == '-') f = -1;
+    for (;ch >= '0' && ch <= '9';ch = getchar()) in = in * 10 + ch - '0';
+    in = in * f;
+}
+
+vector<int> prm;
+vector<ull> pre,suf;
+
+int M,cas;
+
+ull n,k;
+
+ull dfs(ull rem,int lst,ull mul,ull n) {
+	ull tmp = (rem > M ? suf[n / rem] : pre[rem]) - pre[prm[lst] - 1];
+	ull res = (k + 1) * tmp * mul;
+	for(RG int i = lst;i < (int) prm.size();i++) {
+		int p = prm[i];
+		if(1ull * p * p > rem) break;
+		ull nrem = rem,nmul = mul * (k + 1);
+		for(RG ull q = p;q * p <= rem;q *= p) {
+			res += dfs(nrem /= p,i + 1,nmul,n);
+			nmul += mul * k;
+			res += nmul;
+		}
+	}
+	return res;
+}
+
+inline ull solve(ull n) {
+	M = sqrt(n);
+	pre.clear(),suf.clear(),prm.clear();
+	pre.resize(M + 1),suf.resize(M + 1);
+
+	for(RG int i = 1;i <= M;i++) {
+		pre[i] = i - 1,suf[i] = n / i - 1;
+	}
+
+	for(RG int p = 2;p <= M;p++) {
+		if(pre[p] == pre[p - 1]) continue;
+		prm.push_back(p);
+		ull pcnt = pre[p - 1],q = 1ull * p * p,m = n / p;
+		ull end = min(1ull * M,n / q);
+		for(RG int i = 1,w = M / p;i <= w;i++) suf[i] -= suf[i * p] - pcnt;
+		for(RG int i = M / p + 1;i <= end;i++) suf[i] -= pre[m / i] - pcnt;
+		for(RG int i = M;i >= q;i--) pre[i] -= pre[i / p] - pcnt;
+	}
+	
+	prm.push_back(M + 1);
+	return n > 1 ? 1 + dfs(n,0,1,n) : 1;
+}
+
+int main() {
+	read(cas);
+	k = 2;
+	while(cas--) {
+		read(n);
+		printf("%llu\n",solve(n));
+	}
+
+	return 0;
+}
